46d26ba205ff1350d26a1fa7436c705e6cd44b4b
Thanzeel Hassan <thanzeelhassan@gmail.com>
2023-04-06 17:34:53 +0530

    06-04-2023

---
+++ 2-global.js
@@ -0,0 +1,14 @@
+// GLOBALS
+
+// __dirname
+// __filename
+// require
+// module
+// process
+
+console.log(__dirname);
+console.log(__filename);
+
+setInterval(() => {
+  console.log("hello world");
+}, 1000);
